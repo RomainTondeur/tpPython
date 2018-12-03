a82452fcb9f8e0805304871c100e814c91fec2a8
Rom1 Rom2 Rom3 Rom69slt <romain.tond@gmail.com>
2018-12-03 14:13:23 +0100

    Ajout: Fonctions main et Exo 1
    Initialisation de saisie, du main, de la fonction graphAlea, du test entre N et M + Code utilisable pour le peu d'ajout actuel

--- tpGraphe.py
+++ tpGraphe.py
@@ -1,3 +1,17 @@
+#!/usr/bin/python
 import os
 import sys
 
+def graphAlea(n,m):
+	if(n<m):
+		print("Erreur, M est plus grand que N")
+	else:
+		print("Saisie Valide !")
+
+def main():
+	n = input("Saisissez N : ")
+	m= input("Saisissez M : ")
+	graphAlea(n,m)
+
+
+main()
