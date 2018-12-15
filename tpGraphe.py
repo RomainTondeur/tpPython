ac95a86a9b1478e6bb83ad743060529e4c3a67a5
LAMPE Ronan <lampe.ronan@outlook.fr>
2018-12-15 16:20:50 +0100

    core: Implémentation Bellman-Ford

--- tpGraphe.py
+++ tpGraphe.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
 import random
+import math
 import pprint
 
 # Nombre de sommets
 nSom = 0
-# Nombre d'arêtes
+# Nombre d'arcs
 mArc = 0
 # Matrice d'adjacence
 matAdj = []
@@ -62,13 +63,51 @@ def randGraph():
 		numSomOri = random.randint(min(range(nSom)), max(range(nSom)))
 		numSomDest = random.randint(min(range(nSom)), max(range(nSom)))
 		if matAdj[numSomOri][numSomDest] == 0:
-			matAdj[numSomOri][numSomDest] = random.randint(1, 9)
+			matAdj[numSomOri][numSomDest] = random.randint(-9, 9)
 			mArcRest -= 1
 
 	matToLst()
 
 
-# def varianteBellmanFord():
+def bellmanFord(indS):
+	meilDistance = [math.inf for _ in range(nSom)]
+	meilDistance[indS] = 0
+	cpMeilDistance = meilDistance
+
+	for _ in range(nSom - 1):
+
+		# Pour chaque liste de successeurs d'un sommet
+		for indSommet in range(len(matAdj)):
+
+			for indFils in range(len(matAdj[indSommet])):
+
+				# S'il existe un arc pour ce sommet
+				if matAdj[indSommet][indFils] is not 0:
+
+					# Recherche du plus petit coût
+					if meilDistance[indSommet] + matAdj[indSommet][indFils] < meilDistance[indFils]:
+						meilDistance[indFils] = meilDistance[indSommet] + matAdj[indSommet][indFils]
+
+		if cpMeilDistance == meilDistance:
+			break
+		else:
+			cpMeilDistance = meilDistance
+
+	for _ in range(nSom - 1):
+
+		# Pour chaque liste de successeurs d'un sommet
+		for indSommet in range(len(matAdj)):
+
+			for indFils in range(len(matAdj[indSommet])):
+
+				# S'il existe un arc pour ce sommet
+				if matAdj[indSommet][indFils] is not 0:
+
+					# Recherche de circuit absorbant
+					if meilDistance[indSommet] + matAdj[indSommet][indFils] < meilDistance[indFils]:
+						return 1, meilDistance
+
+	return 0, meilDistance
 
 
 # Procédure naïve de Dijkstra
@@ -105,5 +144,12 @@ def main():
 
 	print("\nListe d'adjacence:\n[" + str(lstAdj[0]) + ",\n " + str(lstAdj[1]) + "]")
 
+	indS = 0
+	meilDistance = bellmanFord(indS)
 
+	if meilDistance[0]:
+		print("\nErreur: Circuit absorbant détecté")
+
+	print("\nBellman-Ford - Sommet " + str(indS) + ":")
+	pprint.pprint(meilDistance[1])
 main()
