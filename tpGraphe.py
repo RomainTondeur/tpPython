73a0f37fcd644edd358711f6e0adff54ee2282a1
LAMPE Ronan <lampe.ronan@outlook.fr>
2018-12-17 04:24:13 +0100

    core: Implémentation partielle Dijkstra

--- tpGraphe.py
+++ tpGraphe.py
@@ -2,122 +2,186 @@
 
 import random
 import math
+import heapq
 import pprint
 
-# Nombre de sommets
-nSom = 0
-# Nombre d'arcs
-mArc = 0
-# Matrice d'adjacence
-matAdj = []
-# Liste d'adjacence
-lstAdj = [[], []]
+
+def initMain():
+    while True:
+        nbSommets = int(input("Saisissez N (nombre de sommets): "))
+        nbArcs = int(input("Saisissez M (nombre d'arcs): "))
+        minPond = int(input("Saisissez le minimum de la pondération des arcs: "))
+        maxPond = int(input("Saisissez le maximum de la pondération des arcs: "))
+        indSommet = random.randint(min(range(nbSommets)), max(range(nbSommets)))
+        if (nbArcs <= nbSommets * (nbSommets - 1)) and (nbArcs > 0):
+            return nbSommets, nbArcs, minPond, maxPond, indSommet
+        print("Erreur: Nombre d'arcs trop important")
+
+
+# Procédure de génération d'un graphe (matrice d'adjacence)
+def randGraph(nbSommets, nbArcs, minPond, maxPond):
+
+    print("\nGénération du graphe orienté de " + str(nbSommets) + " sommets et " + str(nbArcs) + " arcs..")
+
+    # Matrice d'adjacence
+    matrice = [[0 for _ in range(nbSommets)] for _ in range(nbSommets)]
+    mArcRests = nbArcs
+    while mArcRests > 0:
+        random.seed()
+        numSomOri = random.randint(min(range(nbSommets)), max(range(nbSommets)))
+        numSomDest = random.randint(min(range(nbSommets)), max(range(nbSommets)))
+        if not matrice[numSomOri][numSomDest]:
+            matrice[numSomOri][numSomDest] = random.randint(minPond, maxPond)
+            mArcRests -= 1
+
+    return matrice
 
 
 # Procédure de translation d'une matrice d'adjacence en liste d'adjacence
-def matToLst():
-	global lstAdj
+def matToLst(matrice):
 
-	# Nombre total de successeurs (indice du dernier successeur connu)
-	nbFils = -1
+    # Liste d'adjacence
+    liste = [[],[]]
 
-	# Pour chaque liste de successeurs d'un sommet
-	for lstFils in matAdj:
+    # Nombre total de successeurs (indice du dernier successeur connu)
+    nbFils = -1
 
-		premFils = 1
-		for indFils in range(len(lstFils)):
+    # Pour chaque liste de successeurs d'un sommet
+    for lstFils in matrice:
 
-			# S'il existe un arc pour ce sommet -> successeur
-			if lstFils[indFils] is not 0:
+        premFils = 1
+        for indFils in range(len(lstFils)):
 
-				# On ajoute l'indice du successeur à la liste des successeurs
-				lstAdj[1].append(indFils)
+            # S'il existe un arc pour ce sommet -> successeur
+            if lstFils[indFils] is not 0:
 
-				# On incrémente le nombre total de successeurs
-				nbFils += 1
+                # On ajoute l'indice du successeur à la liste des successeurs
+                liste[1].append([indFils, lstFils[indFils]])
 
-				# Si le successeur est le premier de ce sommet
-				if premFils:
+                # On incrémente le nombre total de successeurs
+                nbFils += 1
 
-					# On ajoute son indice à la liste des têtes
-					lstAdj[0].append(nbFils)
-					premFils = 0
+                # Si le successeur est le premier de ce sommet
+                if premFils:
 
-		# Si aucun arc n'existe pour ce sommet
-		if premFils:
+                    # On ajoute son indice à la liste des têtes
+                    liste[0].append(nbFils)
+                    premFils = 0
 
-			# On ajoute l'indice du prochain successeur à la liste des têtes
-			lstAdj[0].append(nbFils + 1)
+        # Si aucun arc n'existe pour ce sommet
+        if premFils:
 
-
-# Procédure de génération de graphe
-def randGraph():
-	global matAdj
-
-	print("\nGénération du graphe orienté de " + str(nSom) + " sommets et " + str(mArc) + " arcs..")
-
-	matAdj = [[0 for _ in range(nSom)] for _ in range(nSom)]
-	mArcRest = mArc
-	while mArcRest > 0:
-		random.seed()
-		numSomOri = random.randint(min(range(nSom)), max(range(nSom)))
-		numSomDest = random.randint(min(range(nSom)), max(range(nSom)))
-		if matAdj[numSomOri][numSomDest] == 0:
-			matAdj[numSomOri][numSomDest] = random.randint(-9, 9)
-			mArcRest -= 1
-
-	matToLst()
+            # On ajoute l'indice du prochain successeur à la liste des têtes
+            liste[0].append(nbFils + 1)
+    return liste
 
 
-def bellmanFord(indS):
-	meilDistance = [math.inf for _ in range(nSom)]
-	meilDistance[indS] = 0
-	cpMeilDistance = meilDistance
+# Procédure de retour de la liste des successeurs d'un sommet donné
+def lstFilsSommet(liste, indSommet):
+    if 0 < indSommet < len(liste[0])-1:
+        if liste[0][indSommet] == liste[0][indSommet+1]:
+            return []
+        return liste[1][liste[0][indSommet]:liste[0][indSommet+1]]
+    elif indSommet == 0:
+        return liste[1][:liste[0][indSommet+1]]
+    elif indSommet == len(liste[0])-1:
+        if liste[0][indSommet] == len(liste[1]):
+            return []
+        return liste[1][liste[0][indSommet]:]
 
-	for _ in range(nSom - 1):
 
-		# Pour chaque liste de successeurs d'un sommet
-		for indSommet in range(len(matAdj)):
+def bellmanFord(liste, indSommet):
+    meilDistance = [math.inf for _ in range(len(liste[0]))]
+    meilDistance[indSommet] = 0
+    cpMeilDistance = meilDistance
 
-			for indFils in range(len(matAdj[indSommet])):
+    for _ in range(len(liste[0]) - 1):
 
-				# S'il existe un arc pour ce sommet
-				if matAdj[indSommet][indFils] is not 0:
+        # Pour chaque liste de successeurs d'un sommet
+        for indSom in range(len(liste[0])):
+            fils = lstFilsSommet(liste, indSom)
+            if len(fils) > 0:
+                for successeur in fils:
 
-					# Recherche du plus petit coût
-					if meilDistance[indSommet] + matAdj[indSommet][indFils] < meilDistance[indFils]:
-						meilDistance[indFils] = meilDistance[indSommet] + matAdj[indSommet][indFils]
+                    # Recherche du plus petit coût
+                    if meilDistance[indSom] + successeur[1] < meilDistance[successeur[0]]:
+                        meilDistance[successeur[0]] = meilDistance[indSom] + successeur[1]
 
-		if cpMeilDistance == meilDistance:
-			break
-		else:
-			cpMeilDistance = meilDistance
+        if cpMeilDistance == meilDistance:
+            break
+        else:
+            cpMeilDistance = meilDistance
 
-	for _ in range(nSom - 1):
+    for _ in range(len(liste[0]) - 1):
 
-		# Pour chaque liste de successeurs d'un sommet
-		for indSommet in range(len(matAdj)):
+        # Pour chaque liste de successeurs d'un sommet
+        for indSom in range(len(liste[0])):
+            fils = lstFilsSommet(liste, indSom)
+            if len(fils) > 0:
+                for successeur in fils:
 
-			for indFils in range(len(matAdj[indSommet])):
+                    # Recherche d'absorbsion
+                    if meilDistance[indSom] + successeur[1] < meilDistance[successeur[0]]:
+                        meilDistance[successeur[0]] = -math.inf
+                        print("\nErreur: Circuit absorbant détecté")
+                        return meilDistance
 
-				# S'il existe un arc pour ce sommet
-				if matAdj[indSommet][indFils] is not 0:
+    return meilDistance
 
-					# Recherche de circuit absorbant
-					if meilDistance[indSommet] + matAdj[indSommet][indFils] < meilDistance[indFils]:
-						return 1, meilDistance
 
-	return 0, meilDistance
+# Procédure de recherche du tuple minimum du tas binaire
+def minimum(tasBinaire):
+    index, minPond = tasBinaire[0][0], tasBinaire[0][1]
+    for combinaison in tasBinaire:
+        if combinaison[1] < minPond:
+            index, minPond = combinaison[0], combinaison[1]
+    return [index, minPond]
 
 
 # Procédure naïve de Dijkstra
-def naiveDijkstra():
-	p = 0
-	v = 0
-	M = nSom
+def naiveDijkstra(liste, indSommet):
+    visite = [0 for _ in range(len(liste[0]))]
+    meilDistance = [math.inf for _ in range(len(liste[0]))]
+    meilDistance[indSommet] = 0
+    tasBinaire = [[indSommet, 0]]
+
+    while len(tasBinaire) != 0:
+        index, minPond = minimum(tasBinaire)
+        tasBinaire.pop(tasBinaire.index(minimum(tasBinaire)))
+        visite[index] = 1
+        fils = lstFilsSommet(liste, index)
+        print(fils)
+        if len(fils) > 0:
+            for successeur in fils:
+                if visite[successeur[0]]:
+                    break
+                if meilDistance[index] + successeur[1] < meilDistance[successeur[0]]:
+                    print("peut être")
+                    meilDistance[successeur[0]] = meilDistance[index] + successeur[1]
+                    tasBinaire.append([successeur[0], meilDistance[index] + successeur[1]])
+    return meilDistance
 
 
-# def prioDijkstra():
+# Procédure prioritaire de Dijkstra
+def prioDijkstra(liste, indSommet):
+    visite = [0 for _ in  range(len(liste[0]))]
+    meilDistance = [math.inf for _ in range(len(liste[0]))]
+    meilDistance[indSommet] = 0
+    tasBinaire = [[indSommet, 0]]
+    heapq.heapify(tasBinaire)
+
+    while len(tasBinaire) != 0:
+        index, minPond = heapq.heappop(tasBinaire)
+        visite[index] = 1
+        fils = lstFilsSommet(liste, index)
+        if len(fils) > 0:
+            for successeur in fils:
+                if visite[successeur[0]]:
+                    break
+                if meilDistance[index] + successeur[1] < meilDistance[successeur[0]]:
+                    meilDistance[successeur[0]] = meilDistance[index] + successeur[1]
+                    heapq.heappush(tasBinaire, ([successeur[0], meilDistance[index] + successeur[1]]))
+    return meilDistance
 
 
 # def compaDijkstra():
@@ -127,29 +191,40 @@ def naiveDijkstra():
 
 
 def main():
-	global nSom
-	global mArc
+    # Nombre de sommets et d'arcs
+    nbSommet, nbArc, minPond, maxPond, indSommet = initMain()
 
-	while True:
-		nSom = int(input("Saisissez N (nombre de sommets): "))
-		mArc = int(input("Saisissez M (nombre d'arcs)    : "))
-		if (mArc <= nSom * (nSom - 1)) and (mArc > 0):
-			break
-		print("Erreur: Nombre d'arcs trop important")
+    # Matrice d'adjacence
+    matriceAdjacence = randGraph(nbSommet, nbArc, minPond, maxPond)
 
-	randGraph()
+    print("\nMatrice d'adjacence:")
+    pprint.pprint(matriceAdjacence)
 
-	print("\nMatrice d'adjacence:")
-	pprint.pprint(matAdj)
+    # Liste d'adjacence
+    listeAdjacence = matToLst(matriceAdjacence)
 
-	print("\nListe d'adjacence:\n[" + str(lstAdj[0]) + ",\n " + str(lstAdj[1]) + "]")
+    print("\nListe d'adjacence:\n[" + str(listeAdjacence[0]) + ",\n " + str(listeAdjacence[1]) + "]")
 
-	indS = 0
-	meilDistance = bellmanFord(indS)
+    print("\nListe des successeurs du sommet " + str(indSommet) + ":")
+    pprint.pprint(lstFilsSommet(listeAdjacence, indSommet))
 
-	if meilDistance[0]:
-		print("\nErreur: Circuit absorbant détecté")
+    # Liste des coûts en distance pour le sommet indSommet
+    lstDistBell = bellmanFord(listeAdjacence, indSommet)
 
-	print("\nBellman-Ford - Sommet " + str(indS) + ":")
-	pprint.pprint(meilDistance[1])
+    print("\nBellman-Ford - Sommet " + str(indSommet) + ":")
+    pprint.pprint(lstDistBell)
+
+    # Liste des coûts en distance pour le sommet indSommet
+    lstDistDijNa = naiveDijkstra(listeAdjacence, indSommet)
+
+    print("\nDijkstra Naïf - Sommet " + str(indSommet) + ":")
+    pprint.pprint(lstDistDijNa)
+
+    # Liste des coûts en distance pour le sommet indSommet
+    lstDistDijPrio = prioDijkstra(listeAdjacence, indSommet)
+
+    print("\nDijkstra Prioritaire - Sommet " + str(indSommet) + ":")
+    pprint.pprint(lstDistDijPrio)
+
+
 main()
